6ad629174c082591847533f3ba67d2873ea09c73
Zad Chin <70809099+Iwanttobeatuna@users.noreply.github.com>
2020-12-06 09:07:24 +0800

    Create hello.py

---
+++ week6/hello.py
@@ -0,0 +1,8 @@
+#import get_string from cs50 library
+from cs50 import get_string
+
+#get the user name
+name = get_string("What's your name? ")
+
+#print out the combined string
+print("hello," + name)
